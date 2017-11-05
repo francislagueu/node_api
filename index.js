2d4b8b977e5dba018e875ec46f0da1e9d59f2701
francislagueu <lagson6@yahoo.fr>
2017-11-05 01:17:35 -0800

    complete api with CRUD capabilities implemented

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/crmdb',{
+    useMongoClient:true
+});
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    res.send(`Node and express server is running on port ${ PORT}.`);
+});
+
+app.listen(PORT, ()=>{
+    console.log(`App is running on port ${PORT}.`);
+})
